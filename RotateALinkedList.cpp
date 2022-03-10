6c1396cd1b19f0ea6363d4339f260e83a5090a6a
Bhanuprakash Eagala <31948251+bhanuprakasheagala@users.noreply.github.com>
2022-03-10 15:20:34 +0530

    From GeeksForGeeks

---
+++ RotateALinkedList.cpp
@@ -0,0 +1,125 @@
+/*
+
+Given a singly linked list of size N. The task is to left-shift the linked list by k nodes, 
+where k is a given positive integer smaller than or equal to length of the linked list. 
+
+Input:
+N = 5
+value[] = {2, 4, 7, 8, 9}
+k = 3
+Output: 8 9 2 4 7
+Explanation:
+Rotate 1: 4 -> 7 -> 8 -> 9 -> 2
+Rotate 2: 7 -> 8 -> 9 -> 2 -> 4
+Rotate 3: 8 -> 9 -> 2 -> 4 -> 7
+
+Input:
+N = 8
+value[] = {1, 2, 3, 4, 5, 6, 7, 8}
+k = 4
+Output: 5 6 7 8 1 2 3 4
+
+@GeeksforGeeks
+*/
+
+	// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node* rotate(Node* head, int k)
+    {
+        // Your code here
+        Node* node1 = head;
+        Node* node2 = head;
+        Node* node3 = NULL;
+        
+        for(int i=0; i<k; i++){
+            node3 = node1;
+            node1 = node1->next;
+        }
+        
+        if(node1 == NULL)
+            return head;
+        head = node1;
+        while(node1 != NULL && (node1->next != NULL)){
+            node1 = node1->next;
+        }
+        node3->next = NULL;
+        node1->next = node2;
+        
+        return head;
+    }
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        Solution ob;
+        head = ob.rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+  // } Driver Code Ends
