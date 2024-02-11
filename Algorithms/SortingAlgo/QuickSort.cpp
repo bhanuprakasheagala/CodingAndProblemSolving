7c61ac9601cf35fc4ce0ca1400ed8b3770ffac55
bhanuprakasheagala <bhanuprakasheagala@gmail.com>
2024-02-11 22:20:06 +0530

    Generic Quick sort

--- Algorithms/SortingAlgo/QuickSort.cpp
+++ Algorithms/SortingAlgo/QuickSort.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <vector>
 
-void printArray(const std::vector<int>& arr, int n) {
+template<typename T>
+void printArray(const std::vector<T>& arr, int n) {
     for(int i=0; i<n; ++i) {
         std::cout << arr[i] << " ";
     }
     std::cout << '\n';
 }
-int partition(std::vector<int>& arr, int l, int r) {
+
+template<typename T>
+int partition(std::vector<T>& arr, int l, int r) {
     int pivot = arr[r];
     int i = l-1;
     for(int j=l; j<r; ++j) {
@@ -20,25 +23,38 @@ int partition(std::vector<int>& arr, int l, int r) {
     return i+1;
 }
 
-void quickSort(std::vector<int>& arr, int l, int r) {
+template<typename T>
+void quickSort(std::vector<T>& arr, int l, int r) {
     if(l < r) {
         int pivotPoint = partition(arr, l, r);
         quickSort(arr, l, pivotPoint-1);
         quickSort(arr, pivotPoint+1, r);
     }
 }
-int main() {
-    std::vector<int> input;
-    int num;
-    for(int i=0; i<8; ++i){
-        std::cin >> num;
-        input.push_back(num);
-    }
-    int n = input.size();
-    printArray(input, n);
-    int l = 0, r = n-1;
-    quickSort(input, l, r);
-    printArray(input, n);
 
-    return 0;
+template<typename T>
+void Sort(std::vector<T>& arr, int n) {
+    int l = 0;
+    int r = n-1;
+    quickSort(arr, l, r);
+    printArray(arr, n);
+}
+
+template<typename T>
+void readInputArray(std::vector<T>& arr, int n) {
+    for(int i=0; i<n; ++i)
+        std::cin >> arr[i];
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> arr1(n);
+    std::vector<float> arr2(n);
+    readInputArray(arr1, n);
+    readInputArray(arr2, n);
+
+    std::cout << "Sorted list(s): \n";
+    Sort(arr1, n);
+    Sort(arr2, n);
 }
